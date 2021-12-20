37f67f0d3fc793730c287f8cade7c2a1d590422f
Wojciech Adamiec <wojciechadamiec99@gmail.com>
2021-12-20 17:22:05 +0100

    Minor Refactor 2

--- mm.c
+++ mm.c
@@ -120,8 +120,8 @@ static void *coalesce(void *bp) {
   // Merge with next block
   else if (prev_alloc && !next_alloc) {
     size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-    PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(bp), PACK(size, FREE));
+    PUT(FTRP(bp), PACK(size, FREE));
     // printf("Coalesce with next merge\n");
     return bp;
   }
@@ -130,8 +130,8 @@ static void *coalesce(void *bp) {
   else if (!prev_alloc && next_alloc) {
     set_prev_alloc(NEXT_BLKP(bp), FREE);
     size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-    PUT(FTRP(bp), PACK(size, 0));
-    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, FREE));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, FREE));
     bp = PREV_BLKP(bp);
     // printf("Coalesce with prev merge\n");
   }
@@ -139,8 +139,8 @@ static void *coalesce(void *bp) {
   // Full merge
   else {
     size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
-    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, FREE));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, FREE));
     bp = PREV_BLKP(bp);
     // printf("Coalesce with full merge\n");
   }
@@ -163,9 +163,9 @@ static void *extend_heap(size_t words) {
     return NULL;
 
   // Initialize new free block header/footer and the epilogue header
-  PUT(HDRP(bp), PACK_WITH_PREV(size, 0, last_prev_alloc));
-  PUT(FTRP(bp), PACK_WITH_PREV(size, 0, last_prev_alloc));
-  PUT(HDRP(NEXT_BLKP(bp)), PACK_WITH_PREV(0, 1, 0));
+  PUT(HDRP(bp), PACK_WITH_PREV(size, FREE, last_prev_alloc));
+  PUT(FTRP(bp), PACK_WITH_PREV(size, FREE, last_prev_alloc));
+  PUT(HDRP(NEXT_BLKP(bp)), PACK_WITH_PREV(0, ALLOCATED, FREE));
 
   // printf("Epilogue header pointer=%p\n", HDRP(NEXT_BLKP(bp)));
   // printf("Heap size=%li, Heap start=%p, Heap end=%p\n", mem_heapsize(),
@@ -199,13 +199,13 @@ static void place(void *bp, size_t asize) {
 
   // If free block is big enough make a split
   if ((csize - asize) >= (ALIGNMENT)) {
-    PUT(HDRP(bp), PACK_WITH_PREV(asize, 1, 1));
+    PUT(HDRP(bp), PACK_WITH_PREV(asize, ALLOCATED, ALLOCATED));
     bp = NEXT_BLKP(bp);
-    PUT(HDRP(bp), PACK_WITH_PREV(csize - asize, 0, 1));
-    PUT(FTRP(bp), PACK_WITH_PREV(csize - asize, 0, 1));
+    PUT(HDRP(bp), PACK_WITH_PREV(csize - asize, FREE, ALLOCATED));
+    PUT(FTRP(bp), PACK_WITH_PREV(csize - asize, FREE, ALLOCATED));
     // printf("Place with split, next bp=%p\n", bp);
   } else {
-    PUT(HDRP(bp), PACK_WITH_PREV(csize, 1, 1));
+    PUT(HDRP(bp), PACK_WITH_PREV(csize, ALLOCATED, ALLOCATED));
     set_prev_alloc(NEXT_BLKP(bp), ALLOCATED);
     // printf("Place without split\n");
   }
@@ -220,9 +220,10 @@ int mm_init(void) {
   if ((heap_listp = mem_sbrk(ALIGNMENT)) == (void *)-1)
     return -1;
 
-  PUT(heap_listp, 0);                                     // Alignment padding
-  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));          // Prologue header
-  PUT(heap_listp + (3 * WSIZE), PACK_WITH_PREV(0, 1, 1)); // Epilogue header
+  PUT(heap_listp, 0);                                    // Alignment padding
+  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, ALLOCATED)); // Prologue header
+  PUT(heap_listp + (3 * WSIZE),
+      PACK_WITH_PREV(0, ALLOCATED, ALLOCATED)); // Epilogue header
 
   // printf("Init first 4 bytes=%i\n", GET_BYTES(heap_listp));
   // printf("Init second 4 bytes=%i\n", GET_BYTES(heap_listp + (1 * WSIZE)));
@@ -301,11 +302,9 @@ void *realloc(void *old_ptr, size_t size) {
 
   // Adjust block size to include overhead and alignment reqs
   size_t asize = get_adjusted_size(size);
-
   size_t old_size = GET_SIZE(HDRP(old_ptr));
 
-  /* If old_size is much bigger than current size we make a split */
-
+  // If old_size is much bigger than current size we make a split
   if (old_size >= ALIGNMENT + asize) {
     // printf("Realloc old_size - asize >= ALIGNMENT, asize=%li,
     // old_size=%li\n", asize, old_size);
@@ -318,13 +317,13 @@ void *realloc(void *old_ptr, size_t size) {
     return old_ptr;
   }
 
-  /* If old_size is slightly bigger than current size we do nothing */
+  // If old_size is slightly bigger than current size we do nothing
   if (old_size >= asize) {
     // printf("Realloc old_size - asize >= 0\n");
     return old_ptr;
   }
 
-  /* If is last block make extend_heap */
+  // If our block is last block in heap we make extend_heap */
   if (is_block_last(old_ptr)) {
     size_t extendsize = get_extendsize(asize - old_size);
     last_prev_alloc = ALLOCATED;
@@ -332,11 +331,11 @@ void *realloc(void *old_ptr, size_t size) {
       return NULL;
   }
 
-  /* If new_size is bigger than old one we try to do a merge with next block */
+  // If next block is FREE we try to use it
   if (GET_ALLOC(HDRP(NEXT_BLKP(old_ptr))) == FREE) {
     size_t next_size = GET_SIZE(HDRP(NEXT_BLKP(old_ptr)));
 
-    /* Next block is last block, but it is too small */
+    // If next block (FREE one) is last in heap, but too small we extend it
     if (is_block_last(NEXT_BLKP(old_ptr)) && (old_size + next_size < asize)) {
       size_t extendsize = get_extendsize(asize - old_size - next_size);
       last_prev_alloc = FREE;
@@ -344,7 +343,7 @@ void *realloc(void *old_ptr, size_t size) {
         return NULL;
     }
     // printf("Realloc next block is FREE, next_size=%li\n", next_size);
-    /* We do merge with split if possible */
+    // We do merge with split if possible
     if (old_size + next_size >= ALIGNMENT + asize) {
       size_t prev_alloc = GET_PREV_ALLOC(HDRP(old_ptr));
       PUT(HDRP(old_ptr), PACK_WITH_PREV(asize, ALLOCATED, prev_alloc));
@@ -359,7 +358,7 @@ void *realloc(void *old_ptr, size_t size) {
       return old_ptr;
     }
 
-    /* Simple merge */
+    // We merge without split
     if (old_size + next_size >= asize) {
       // printf("Realloc simple merge\n");
       size_t prev_alloc = GET_PREV_ALLOC(HDRP(old_ptr));
@@ -371,17 +370,17 @@ void *realloc(void *old_ptr, size_t size) {
   }
 
   // printf("Realloc copy\n");
-  /* If we cannot perform anything special we just make a alloc a new block and
-   * copy data */
+  // If we cannot perform anything optimal we just make alloc a new block and
+  // copy data
   void *new_ptr = malloc(size);
 
-  /* If malloc() fails, the original block is left untouched. */
+  // If malloc fails, the original block is left untouched
   if (!new_ptr)
     return NULL;
 
   memcpy(new_ptr, old_ptr, old_size);
 
-  /* Free the old block. */
+  // Free the old block
   free(old_ptr);
 
   return new_ptr;
@@ -429,7 +428,6 @@ void mm_checkheap(int verbose) {
       printf("[%i] FOOT: %p size: %i %s %s\n", i, bp, GET_SIZE(FTRP(bp)),
              s_ft_alloc, s_ft_prev_alloc);
     }
-
     i++;
   }
   return;
